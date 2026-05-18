ebb01727a88ebca50b28bec0c6fea8a032cf1a72
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-18 13:32:00 -0300

    feat: implement MySQL database connection and update server startup logic

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,19 @@
+require("dotenv").config()
+
 const app = require('./src/app');
+const db = require('./src/config/db')
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-});
+db.getConnection()
+  .then(() =>{
+    console.log("MySQL connected")
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    });
+  })
+
+  .catch((err) =>{
+    console.log("Database connection error: ",err)
+  })
