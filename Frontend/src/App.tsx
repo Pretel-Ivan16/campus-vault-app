f0a3f27613515cba212a9bb767bbdb4155a7e561
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-20 16:04:22 -0300

    feat: update routing in App component to set Home as the default page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
