9cd8826439aa87d75a4d2fdbaf32a3ceb4b97403
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-05-22 12:43:50 -0300

    refactor: simplify layout component by removing unused imports and classes

--- campus-vault-app/src/app/layout.tsx
+++ campus-vault-app/src/app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,10 +6,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
