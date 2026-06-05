05d9526d38c8a2b6d2e01d08dc0e198083f04630
Iván Pretel <gonzalo2004.2017@gmail.com>
2026-06-05 11:01:32 -0300

    feat: add sign-up schema validation using zod

---
+++ campus-vault/src/features/auth/schemas/sign-up-schema.ts
@@ -0,0 +1,17 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  name: z
+    .string()
+    .min(3),
+  
+  email: z
+    .email(),
+  
+  password: z
+    .string()
+    .min(8),
+});
+
+export type SignUpInput =
+  z.infer<typeof signUpSchema>;
